52319fd6fe0bf7ffe525afed37a87c6f19caa5b5
kellysong1 <149431845+kk4song@users.noreply.github.com>
2025-01-06 13:50:44 -0500

    hero done but still some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Hero from './components/Hero.jsx'
+import About from './components/About.jsx'
 
 
 const App = () => {
@@ -7,6 +8,8 @@ const App = () => {
     <main className = "relative min-h-screen w-screen overflow-x-hidden">
       <h1>
         <Hero/>
+
+        <About/>
       </h1>
     </main>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,38 +4,111 @@ import Button from "./Button";
 
 import React, { useRef, useState, useEffect } from 'react'
 
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+import {ScrollTrigger} from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
+
 
 const Hero = () => {
 
     const [currentIndex, setCurrentIndex] = useState(1);
 
     const [hasClicked, setHasClicked] = useState(false);
-    const [loading, setLoading] = useState(true);
+    const [loading, setIsLoading] = useState(true);
     const [loadedVideos, setLoadedVideos] = useState(0);
 
 
-    const totalVideos = 3;
+    const totalVideos = 4;
 
     const nextVideoRef = useRef(null);
 
     const handleVideoLoad = () => {
-        setLoadedVideos((prev) => prev + 1);
+      setLoadedVideos((prev) => prev + 1);
+    };
+  
+    useEffect(() => {
+      if (loadedVideos === totalVideos - 1) {
+        setIsLoading(false);
+      }
+    }, [loadedVideos]);
+  
+    const handleMiniVdClick = () => {
+      setHasClicked(true);
+  
+      setCurrentIndex((prevIndex) => (prevIndex % totalVideos) + 1);
     };
 
-    const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+      useEffect(() => {
+        if(loadedVideos === totalVideos - 1) {
+          setIsLoading(false)
+        }
+      }, [loadedVideos])
 
-    const handleMiniVdClick = () => {
 
-        setHasClicked(true);
-        setCurrentIndex(upcomingVideoIndex);
+      useGSAP(() => {
+        
+        if(hasClicked) {
+          gsap.set('#next-video', {visibility: 'visible'});
+          gsap.to("#next-video", {
+            transformOrigin: "center center",
+            scale: 1,
+            width: "100%",
+            height: "100%",
+            duration: 1,
+            ease: "power1.inOut",
+            onStart: () => nextVideoRef.current.play()
+          });
+          gsap.from("#current-video", {
+            transformOrigin: "center center",
+            scale: 0,
+            duration: 1.5,
+            ease: "power1.inOut",
+          });
 
-      };
+
+        }
+      }, {dependencies: [currentIndex], revertOnUpdate: true})
+
+      useGSAP(() => {
+        gsap.set("#video-frame", {
+          clipPath: "polygon(14% 0, 72% 0, 88% 90%, 0 95%)",
+          borderRadius: "0% 0% 40% 10%",
+        });
+        gsap.from("#video-frame", {
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          borderRadius: "0% 0% 0% 0%",
+          ease: "power1.inOut",
+          scrollTrigger: {
+            trigger: "#video-frame",
+            start: "center center",
+            end: "bottom center",
+            scrub: true,
+          },
+        });
+      });
+
+
 
       const getVideoSrc = (index) => `videos/spy-${index}.mp4`;
 
 
   return (
     <div className = "relative h-dvh w-screen overflow-x-hidden">
+
+{loading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          <div className="three-body">
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+          </div>
+        </div>
+      )}
+
+
         <div id= "video-frame" className = "relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
             <div>
                 <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
@@ -52,7 +125,7 @@ const Hero = () => {
                     </div>
                 </div>
 
-            <video
+                <video
             ref={nextVideoRef}
             src={getVideoSrc(currentIndex)}
             loop
@@ -60,9 +133,9 @@ const Hero = () => {
             id="next-video"
             className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
             onLoadedData={handleVideoLoad}
-            />
+          />
 
-        <video
+          <video
             src={getVideoSrc(
               currentIndex === totalVideos - 1 ? 1 : currentIndex
             )}
@@ -75,7 +148,9 @@ const Hero = () => {
 
             </div>
 
-        
+            <h1 className = "special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+                G<b>a</b>ming
+        </h1>
 
             <div className = "absolute left-0 top-0 z-40 size-full">
 
@@ -91,9 +166,9 @@ const Hero = () => {
             </div>
         </div>
 
-        <h1 className = "special-font hero-heading absolute bottom-5 right-5 z-40 text-black">
-                F<b>a</b>mily
-        </h1>
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
+        G<b>A</b>MING
+      </h1>
 
     </div>
   )
