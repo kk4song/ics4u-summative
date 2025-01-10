d4a2459cdadc47e06fb770522e8acafec1c0547d
kellysong1 <149431845+kk4song@users.noreply.github.com>
2025-01-10 14:47:21 -0500

    updates!!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Hero from './components/Hero.jsx'
 import About from './components/About.jsx'
+import Navbar from './components/Navbar.jsx'
 
 
 const App = () => {
   return (
     <main className = "relative min-h-screen w-screen overflow-x-hidden">
       <h1>
+        <Navbar/>
         <Hero/>
 
         <About/>
+
       </h1>
     </main>
   )
